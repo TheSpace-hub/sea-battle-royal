71725293e17ba05230306403cebb8696ef449416
TheSpace-hub <biometer2020@gmail.com>
2026-01-31 18:10:23 +0300

    Initial commit

---
+++ src/main/java/ru/seabattleroyal/seabattleroyal/SeaBattleRoyalApplication.java
@@ -0,0 +1,13 @@
+package ru.seabattleroyal.seabattleroyal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaBattleRoyalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeaBattleRoyalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/seabattleroyal/seabattleroyal/SeaBattleRoyalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.seabattleroyal.seabattleroyal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaBattleRoyalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
