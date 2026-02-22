579d4917f613106733061ee590d9138cff4c2308
TheSpace-hub <biometer2020@gmail.com>
2026-02-22 09:11:24 +0300

    Add players

--- app/src/main/java/ru/seabattleroyal/game/Game.java
+++ app/src/main/java/ru/seabattleroyal/game/Game.java
@@ -1,10 +1,30 @@
 package ru.seabattleroyal.game;
 
-import java.util.Random;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+@Getter
+@Setter
 public class Game {
 
     private static final Random random = new Random();
+
+    private final Set<Player> players = new HashSet<>();
+
+    public Game() {
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public void removePlayer(Player player) {
+        players.remove(player);
+    }
 
     public static String generateId() {
         char[] code = new char[6];
