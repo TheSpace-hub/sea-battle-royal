4ad239b839d65aa396ca93d8e3445205532249a0
TheSpace-hub <biometer2020@gmail.com>
2026-02-22 16:11:27 +0300

    Create basic classes

---
+++ app/src/main/resources/static/js/game/index.ts
@@ -0,0 +1,35 @@
+class Player {
+    private readonly _username: string
+    public field: Field
+
+    constructor(username: string) {
+        this._username = username
+        this.field = new Field()
+    }
+
+}
+
+class Field {
+    private readonly _field: CellType[][]
+
+    constructor() {
+        this._field = Array.from({length: 10}, () => Array(10).fill(CellType.UNKNOWN))
+    }
+
+    public setCell(x: number, y: number, type: CellType) {
+        // @ts-ignore
+        this._field[y][x] = type
+    }
+
+    public getCell(x: number, y: number): CellType {
+        // @ts-ignore
+        return this._field[y][x]
+    }
+}
+
+enum CellType {
+    UNKNOWN,
+    EMPTY,
+    WOUNDED,
+    DEAD
+}
