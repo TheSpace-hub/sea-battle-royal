91f7c3e40ced55fec056b06ad1973242960f5ea1
TheSpace-hub <biometer2020@gmail.com>
2026-02-22 07:16:53 +0300

    Basic create game

--- app/src/main/java/ru/seabattleroyal/App.java
+++ app/src/main/java/ru/seabattleroyal/App.java
@@ -2,18 +2,26 @@ package ru.seabattleroyal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/api")
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
 
-    public String index(Model model) {
-        return "index";
+    @PostMapping("/create-game")
+    public Map<String, Integer> createGame(@RequestBody Map<String, Integer> body) {
+        System.out.println(body);
+        System.out.println(body.get("number-of-players"));
+        return Map.of("id", 0);
     }
 
 }
