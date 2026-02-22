dea876d1e5837ef32c09158d6a3c2408f13f0a23
TheSpace-hub <biometer2020@gmail.com>
2026-02-22 09:08:06 +0300

    Basic field class

---
+++ app/src/main/java/ru/seabattleroyal/game/Field.java
@@ -0,0 +1,34 @@
+package ru.seabattleroyal.game;
+
+public class Field {
+
+    private CellType[][] field;
+
+    public Field(CellType defaultField) {
+        field = new CellType[10][10];
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                field[y][x] = defaultField;
+            }
+        }
+    }
+
+    public Field(CellType[][] field) {
+        this.field = field;
+    }
+
+    public void setCell(int x, int y, CellType cell) {
+        field[y][x] = cell;
+    }
+
+    public CellType getCell(int x, int y) {
+        return field[y][x];
+    }
+
+    public enum CellType {
+        UNKNOWN,
+        EMPTY,
+        WOUNDED,
+        DEAD
+    }
+}
