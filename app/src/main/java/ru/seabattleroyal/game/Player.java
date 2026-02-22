7cfe2b45197cc9ee1b1c766f7e58667fa877a728
TheSpace-hub <biometer2020@gmail.com>
2026-02-22 09:11:18 +0300

    Add field

--- app/src/main/java/ru/seabattleroyal/game/Player.java
+++ app/src/main/java/ru/seabattleroyal/game/Player.java
@@ -1,10 +1,22 @@
 package ru.seabattleroyal.game;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.UUID;
 
+@Getter
+@Setter
 public class Player {
 
     String username;
     UUID uuid;
+    Field field;
+
+    public Player(String username) {
+        this.username = username;
+        this.uuid = UUID.randomUUID();
+        this.field = new Field(Field.CellType.UNKNOWN);
+    }
 
 }
