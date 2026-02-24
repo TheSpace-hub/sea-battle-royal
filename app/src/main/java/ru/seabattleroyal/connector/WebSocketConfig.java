64e6ca6a748e84cd1f7c097c110a6ed9a0e34cd6
TheSpace-hub <biometer2020@gmail.com>
2026-02-24 20:06:41 +0300

    Join action

--- app/src/main/java/ru/seabattleroyal/connector/WebSocketConfig.java
+++ app/src/main/java/ru/seabattleroyal/connector/WebSocketConfig.java
@@ -6,17 +6,14 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import ru.seabattleroyal.repositories.GameRepository;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    private final GameRepository repository;
     private final AuthChannelInterceptor authChannelInterceptor;
 
-    public WebSocketConfig(GameRepository repository, AuthChannelInterceptor authChannelInterceptor) {
-        this.repository = repository;
+    public WebSocketConfig(AuthChannelInterceptor authChannelInterceptor) {
         this.authChannelInterceptor = authChannelInterceptor;
     }
 
