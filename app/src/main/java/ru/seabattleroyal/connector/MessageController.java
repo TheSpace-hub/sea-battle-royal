aefae2780677a21e84610eca13e0dfd302fa58fa
TheSpace-hub <biometer2020@gmail.com>
2026-02-25 23:19:23 +0300

    Send info about players

---
+++ app/src/main/java/ru/seabattleroyal/connector/MessageController.java
@@ -0,0 +1,45 @@
+package ru.seabattleroyal.connector;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import ru.seabattleroyal.game.Game;
+import ru.seabattleroyal.game.Player;
+import ru.seabattleroyal.repositories.GameRepository;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Controller
+public class MessageController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final GameRepository repository;
+
+    public MessageController(SimpMessagingTemplate messagingTemplate, GameRepository repository) {
+        this.messagingTemplate = messagingTemplate;
+        this.repository = repository;
+    }
+
+    @MessageMapping("/info-is-needed")
+    public void infoIsNeeded(@Payload String gameId) {
+        log.info("info-is-needed for game {}", gameId);
+        Game game = repository.getGame(gameId);
+        if (game == null) {
+            return;
+        }
+
+        Map<String, String> players = new HashMap<>();
+        for (Player player : game.getPlayers()) {
+            players.put(player.getUuid(), player.getUsername());
+        }
+        messagingTemplate.convertAndSend("/topic/game." + gameId + ".information-about-players",
+                mapper.writeValueAsString(players));
+    }
+
+}
