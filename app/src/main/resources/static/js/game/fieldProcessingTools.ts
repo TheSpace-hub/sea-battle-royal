e7af5a84a1e72dc816016ec010c3e5bc7bfc3edb
TheSpace-hub <biometer2020@gmail.com>
2026-02-27 18:06:54 +0300

    Basic logic

---
+++ app/src/main/resources/static/js/game/fieldProcessingTools.ts
@@ -0,0 +1,46 @@
+import {CellType, type Field, Position} from "./index.js";
+
+export function getShipsSet(field: Field): Set<Set<Position>> {
+    const ships: Set<Set<Position>> = new Set<Set<Position>>()
+    for (let y = 0; y < field.sizeY; y++) {
+        for (let x = 0; x < field.sizeX; x++) {
+            let found: boolean = false;
+            for (let ship of ships) {
+                for (let cell of ship) {
+                    if (cell.x == x && cell.y == y) {
+                        found = true
+                        break
+                    }
+                }
+                if (found) continue
+                if (field.getCell(x, y) == CellType.SHIP) {
+                    ships.add(findTheShip(field, x, y))
+                }
+            }
+        }
+    }
+    return ships
+}
+
+function findTheShip(field: Field, x: number, y: number): Set<Position> {
+    const positions = new Set<Position>
+
+    let direction: number = 0
+
+    if (field.getCell(x, y + 1) == CellType.SHIP) {
+        direction = 1
+    } else {
+        positions.add(new Position(x, y))
+        return positions
+    }
+
+    while (field.getCell(x, y) === CellType.SHIP) {
+        positions.add(new Position(x, y))
+        if (direction == 0)
+            x += 1
+        else
+            y += 1
+    }
+
+    return positions
+}
