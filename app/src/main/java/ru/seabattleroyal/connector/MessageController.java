9e3ce58be47dbbf227edd55bb40340731543e469
TheSpace-hub <biometer2020@gmail.com>
2026-02-28 12:02:40 +0300

    Send private field to player

--- app/src/main/java/ru/seabattleroyal/connector/MessageController.java
+++ app/src/main/java/ru/seabattleroyal/connector/MessageController.java
@@ -135,7 +135,9 @@ public class MessageController {
         messagingTemplate.convertAndSend("/topic/game." + gameId + ".attack", "");
         messagingTemplate.convertAndSend("/topic/game." + gameId + ".move", game.getPlayers().get(game.getCurrentPlayerIndex()).getUuid());
         messagingTemplate.convertAndSend("/topic/game." + gameId + ".update-fields", mapper.writeValueAsString(game.getPublicFields()));
-
+        for (Player player : game.getPlayers()) {
+            messagingTemplate.convertAndSend("/topic/private-field." + player.getUuid(), mapper.writeValueAsString(player.getField()));
+        }
     }
 
 }
